b8129d7e365543623a7a09a4797b40844bef059e
Joaco <joaquinnme@outlook.com>
2023-07-10 21:00:09 -0300

    + boton propio +/ cambio de pass

--- src/Controllers/users.controller.js
+++ src/Controllers/users.controller.js
@@ -11,3 +11,19 @@ export const upgradeUser = async (req, res) => {
     res.send({ status: 'Error', message: `User could not be upgrated! Detail: %${error}`})
   }
 }
+
+export const restorePass = async (req, res) => {
+  try {
+    let email = req.body.user
+    if (req.body.pass != req.body.repeat-pass) {
+      res.status(502).json()
+    }
+    let result = await US.restorePass(email, req.body.pass)
+    if(result.status == 503) {
+      res.status(503).json()
+    }
+    res.status(201).send(result)
+  } catch (error) {
+    res.send({ status: 'Error', message: `Password couldn't be changed! Detail: %${error}`})
+  }
+}
