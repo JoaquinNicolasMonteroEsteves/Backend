cb9896507e1e51ac4f96f83ab67cff5511e78399
Joaquín Nicolás Montero Esteves <joaquinnme@outlook.com>
2023-08-24 00:03:07 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import initializePassport from './config/passport.config.js'
 import cookieParser from 'cookie-parser'
 import config from './config/config.js'
 import routerM from './Routes/messages.router.js'
-import routerMocks from './Routes/mock.router.js'
+//import routerMocks from './Routes/mock.router.js'
 import { addLogger } from './config/logger_Base.js'
 import MongoSingleton from './config/mongodb-singleton.js'
 import swaggerUIExpress from 'swagger-ui-express'
@@ -145,4 +145,4 @@ app.use('/api/carts', routerC)
 app.use('/api/messages', routerM)
 app.use('/api/emails', routerE)
 app.use('/github', routerG)
-app.use('/mockingproducts', routerMocks)
+//app.use('/mockingproducts', routerMocks)
